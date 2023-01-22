402489fd7bd07ec7250cd25032e1850199b3a3f2
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2023-01-22 05:08:09 -0500

    almost done!

--- create_persistant_live_usb.sh
+++ create_persistant_live_usb.sh
@@ -23,7 +23,12 @@ cat <<EOF
                             use this function alone, then run the 
                             program again with the expected args for
                             creation of a live USB
-
+  -d, --use_debootstrap     Uses debootstrap to generate an iso of 
+                            Debian that can be tailored to your 
+                            specifications
+  -s, --chroot_script       script to run on chroot when generating a
+                            custom Debian iso file, this must be a 
+                            system path
  Commands:
   -h, --help Displays this help and exits
 
@@ -54,7 +59,8 @@ read -r -d '' empty_required_args_error_message<<'EOF'
 [-]
 [-]     Full system path to a debian live iso, if you dont have one, you can 
 [-]     run this program using the -g or --get_iso option to download one from
-[-]     the official debian mirrors
+[-]     the official debian mirrors. This option is ignored when the option to
+[-]     use "debootstrap" is given. That option creates an iso.
 [-]     
 [-]         example: /home/user/Downloads/debian.iso
 [-]
@@ -120,6 +126,169 @@ temp_persist_dir="/tmp/usb-persistence"
 temp_live_iso_dir="/tmp/live-iso"
 }
 
+#==============================================================================
+#BEGIN CHROOT STUFF
+#==============================================================================
+#----------------------------------------------------------------------------
+# root password for root user in chroot container
+chroot_root_password="password"
+# username for new VM/container/host creations
+chroot_new_username="test"
+# password for new VM/container/host creations
+chroot_new_user_password="password"
+# hostname for new image creation
+
+ARCH='amd64'
+#COMPONENTS='main,contrib'
+REPOSITORY="https://deb.debian.org/debian/"
+# debian version
+release="stable"
+# packages to include DURING debootstrap
+includes="linux-image-amd64,grub-pc,ssh,vim"
+
+# makes an empty disk image using dd
+# to achive correct size in gigabytes, maths out "num_GB/1024"
+# e.g. 5GB * 1024 == 5120
+block_size="1M"
+block_count='5120'
+# use parted syntax
+#image_size='4G'
+image_name="new_debian"
+output_iso="$home_dir/$image_name.iso"
+# location to build distro
+iso_build_folder="$home_dir/iso_build_folder"
+# location to create image
+image_location="$home_dir/$image_name.img"
+# loop device to create
+loop_device="/dev/loop0"
+
+# works, dont change
+make_files()
+{
+# create the build folder
+echo "[+] Creating $iso_build_folder"
+mkdir "$iso_build_folder"
+
+# create the image file
+echo "[+] Creating $image_location"
+touch "$image_location.img"
+}
+
+# works, dont change
+make_disk()
+{
+# fill image file to correct size with all zeros
+dd if=/dev/zero of="$image_location.img" bs=$block_size count=$block_count
+# create loop device out of image file
+sudo losetup $loop_device "$image_location.img"
+# create schema
+sudo parted -s $loop_device mklabel gpt
+# create partition
+sudo parted -s $loop_device mkpart primary 1MiB 100%
+# create ext4 filesystem
+echo "y
+" | sudo mkfs.ext4 $loop_device
+# mount loop device on iso build folder
+sudo mount $loop_device "$iso_build_folder"
+}
+
+build_new_os()
+{
+# begin pulling all necessary data for debian install
+# if this fails, depending on the fail it might work if run a second time
+# TODO: check for "E: Couldn't download packages:" and interrupt to run again
+
+# these NEED to be run, you WILL encounter an error if packages mismatch
+sudo apt update -y
+sudo apt upgrade -y 
+
+# begin the pull and install
+sudo debootstrap --arch "$ARCH" --include="$includes" "$release" "$iso_build_folder" "$REPOSITORY"
+# copy files necessary for networking and package managment
+sudo cp /etc/resolv.conf "$iso_build_folder/etc/resolv.conf"
+sudo cp /etc/apt/sources.list "$iso_build_folder/etc/apt/"
+sudo cp /etc/hosts "$iso_build_folder/etc/hosts"
+}
+
+prepare_chroot()
+{
+#mount --make-rslave --rbind /proc /mnt/proc
+#mount --make-rslave --rbind /sys /mnt/sys
+#mount --make-rslave --rbind /dev /mnt/dev
+#mount --make-rslave --rbind /run /mnt/run
+#chroot /mnt /bin/bash
+# mount in preparation for chroot
+sudo mount -o bind /dev "$iso_build_folder/dev"
+sudo mount none -t devpts "$iso_build_folder/dev/pts"
+sudo mount -o bind -t proc /proc "$iso_build_folder/proc"
+sudo mount -o bind -t sys /sys "$iso_build_folder/sys"
+sudo mount --bind /run  "$iso_build_folder/run"
+}
+
+#######################################
+# 
+# param1: optional, bash code to run in
+#         chroot
+#######################################
+chroot_buildup(){
+# ROOT USER ONLY
+# perform scripted actions in chroot
+cat << EOF | sudo chroot "$iso_build_folder"
+
+# change hostname to fit
+echo "$VMHOSTNAME" > /etc/hostname
+
+# add user and set password
+echo "$chroot_new_user_password
+$chroot_new_user_password" | adduser "$chroot_new_username"
+
+# change root password
+echo "$chroot_root_password:$chroot_root_password" | chpasswd
+
+
+# this step should be performed inside and outside the chroot
+apt update
+apt upgrade
+
+# install prerequisites for third party apt installs
+
+apt install --no-install-recommends -y install sudo debconf nano apt-transport-https ca-certificates curl gnupg lsb-release wget curl
+
+#add user to sudoers group
+usermod -aG sudo $chroot_new_username
+EOF
+
+if $1; then
+    chroot_exec_heredoc $1
+fi
+}
+
+teardown_chroot()
+{
+# unmount chroot pipelines
+umount -lf /proc
+umount -lf /sys
+umount -lf /dev/pts
+# detach loop device, now its just a file with no connections or locks
+sudo losetup -d /dev/loop0
+}
+
+create_bootable_iso()
+{
+sudo genisoimage -o "$output_iso" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table /
+
+}
+
+debootstrap_process()
+{
+make_files
+make_disk
+build_new_os
+prepare_chroot
+chroot_buildup
+teardown_chroot
+create_bootable_iso
+}
 # creates /dev/xx1 EFI boot partition
 # This creates the basic disk structure of an EFI disk with a single OS.
 create_EFI()
@@ -397,8 +566,16 @@ sudo rmdir $temp_efi_dir $temp_live_dir $temp_persist_dir $temp_live_iso_dir
 
 # put short options after -o without commas
 # put long options after --long, with commas
-# options that have no short option should have : after them, before the comma
-TEMP_OPTS=$(getopt -o aldgh: --long architecture,iso_path,device,get_new_iso,help -n 'create_live_usb' -- "$@")
+# options that have arguments should have : after them, before the comma
+short_opts="a:i:d:ugh"
+long_opts="architecture:,\
+iso_path:,\
+device:,\
+use_debootstrap\
+get_new_iso,\
+help"
+#TEMP_OPTS=$(getopt -o a:i:d:gh: --long architecture:,iso_path:,device:,get_new_iso:,help -n 'create_live_usb' -- "$@")
+TEMP_OPTS=$(getopt -o ${short_opts} --long ${long_opts} -n 'create_live_usb' -- "$@")
 
 #if [ $? != 0 ] ; then 
 #    echo "Terminating..." >&2 ; exit 1 ; fi
@@ -408,13 +585,14 @@ eval set -- "$TEMP_OPTS"
 
 while true; do
     case "$1" in
-        --a | --architecture)   architecture="$2"; shift ;;
-        --l | --iso_path)       iso_path="$2"; shift ;;
-        --d | --device)         device="$2"; shift ;;
-        --g | --get_new_iso)    get_new_iso ;;
-        --h | --show_help )     show_help ;;
-        --  )                   shift; break ;;
-        *   )                   show_help; break ;;
+        --a | --architecture)    architecture="$2"; shift 2;;
+        --i | --iso_path)        iso_path="$2"; shift 2;;
+        --d | --device)          device="$2"; shift 2;;
+        --g | --get_new_iso)     get_new_iso ;;
+        --u | --use-debootstrap) use_debootstrap=true; shift 2 ;;
+        --h | --show_help)       show_help ;;
+        --  )                    shift; break ;;
+        *   )                    show_help; break ;;
     esac
 done
 #while getopts "a:l:d:g:h:v:" opt
@@ -462,9 +640,18 @@ else
     mount_LIVE
     mount_PERSIST
 
-    # mount ISo file to begin copying data to USB
-    mount_ISO
-
+    # obtaining the data for an OS from one of two sources
+    # whether a premade OS direct from debian mirrors
+    # or using the utility "debootstrap" to create an OS in
+    # a chroot environment that is usually much smaller
+    # and specialized
+    if $use_debootstrap; then
+        # creates an iso using debootstrap to customize the OS
+        debootstrap_process
+    else
+        # mount ISO file to begin copying data to USB
+        mount_ISO
+    fi
     # copy files from mounted iso to temporary work directories
     copy_ISO_to_tmp
 
